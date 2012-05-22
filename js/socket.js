4e14930637339c4c9a7a3cfbb6ced653d99832d1
Tom <github@tomnatt.com>
2012-05-22 19:01:29 +0100

    updated socket code

--- js/socket.js
+++ js/socket.js
@@ -1,5 +1,7 @@
 // SET UP SOCKET
-var socket = new io.Socket(null, {rememberTransport: false, port: 8080});
+
+/*
+var socket = new io.Socket(null, {rememberTransport: false, port: 9000});
 socket.connect();
 socket.addEvent('message', function(data) {
 
@@ -7,6 +9,38 @@ socket.addEvent('message', function(data) {
     
 });
 // END SET UP SOCKET
+*/
+
+$(document).ready(function() {
+    console.log("I'm about to connect");
+    connect();
+});
+
+function connect() {
+    ws = new WebSocket("ws://ccpc-twn.bath.ac.uk:9000/echoSocket?name=TestUser");
+    //ws = new WebSocket("ws://ccpc-twn.bath.ac.uk:9000/helloSocket?name=TestUser");
+
+    ws.onopen = function(evt) { 
+        console.log("connected");
+    }
+
+    ws.onclose = function(evt) {
+        console.log("disconnected");
+    }
+
+    ws.onmessage = function(evt) {
+        console.log("response: " + evt.data);
+    }
+
+    ws.onerror = function(evt) {
+        console.log("error: " + evt.data);
+    }
+}
+
+function disconnect() {
+    ws.close();
+}
+
 
 // is this of any use?
 // http://stackoverflow.com/questions/7919856/how-to-use-websockets-with-play-framework
