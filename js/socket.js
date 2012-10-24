93fa132be89561a53e938369bce7aebb75d22d38
Tom Natt <github@tomnatt.com>
2012-10-24 18:51:35 +0100

    added chat channel support

--- js/socket.js
+++ js/socket.js
@@ -1,20 +1,38 @@
 // SET UP SOCKET
 
 $(document).ready(function() {
-    console.log("I'm about to connect");
-    connectControl();
     
-    connectionReady();
+    console.log("I'm about to connect");
+    
+    // ready the controls
+    connectControl();
+    controlConnectionReady();
+    
+    // ready the talky channel
+    connectChat();
+    chatConnectionReady();
     
 });
 
-count = 0;
-function connectionReady() {
-    if (count > 20) {
-        console.log("giving up");
+controlCount = 0;
+function controlConnectionReady() {
+    if (controlCount > 20) {
+        console.log("control connection giving up");
     } else if (ws1.readyState != 1) {
-        count++;
-        setTimeout(connectionReady, 500);
+        controlCount++;
+        setTimeout(controlConnectionReady, 500);
+    } else {
+        // ready
+    }
+}
+
+chatCount = 0;
+function chatConnectionReady() {
+    if (chatCount > 20) {
+        console.log("chat connection giving up");
+    } else if (ws2.readyState != 1) {
+        chatCount++;
+        setTimeout(chatConnectionReady, 500);
     } else {
         // ready
     }
@@ -25,34 +43,47 @@ function connectControl() {
     ws1 = new WebSocket("ws://localhost:8080/");
 
     ws1.onopen = function(evt) { 
-        console.log("connected");
+        console.log("control connected");
     }
 
     ws1.onclose = function(evt) {
-        console.log("disconnected");
+        console.log("control disconnected");
     }
 
     ws1.onmessage = function(evt) {
-        doSomething(evt.data);
+        motion(evt.data);
     }
 
     ws1.onerror = function(evt) {
-        console.log("error: " + evt.data);
+        console.log("control error: " + evt.data);
     }
     
 }
 
-function disconnect() {
-    ws1.close();
-}
+function connectChat() {
+    ws2 = new WebSocket("ws://localhost:8081/");
 
-function send(words) {
-    ws1.send(words);
+    ws2.onopen = function(evt) { 
+        console.log("chat connected");
+    }
+
+    ws2.onclose = function(evt) {
+        console.log("chat disconnected");
+    }
+
+    ws2.onmessage = function(evt) {
+        chat(evt.data);
+    }
+
+    ws2.onerror = function(evt) {
+        console.log("chat error: " + evt.data);
+    }
+    
 }
 
 // respond to incoming signals
-function doSomething(data) {
-
+function motion(data) {
+    
     if (data == "move up") { 
         // up 
         Reveal.navigateUp();
@@ -69,6 +100,20 @@ function doSomething(data) {
     
 }
 
+queue = new Array();
+function chat(data) {
+    console.log("chat: " + data);
+    
+    // add message to queue
+    queue.push(data);
+}
 
-// is this of any use?
-// http://stackoverflow.com/questions/7919856/how-to-use-websockets-with-play-framework
+$(document).ready(function() {
+    setInterval(function() {
+        if (queue.length > 0) {
+            $('#onTop p').fadeOut(500, function() {
+                $(this).text(queue.shift()).fadeIn(500);
+            });
+        }
+    }, 4000);
+});
